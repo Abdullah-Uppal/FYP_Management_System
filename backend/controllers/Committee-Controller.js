3a4ea5b328a15a63dcf99b0d59aaa642ad612633
Abdullah-Uppal <computerinsider4@gmail.com>
2022-12-20 18:29:29 +0500

    Yar add wala tou meri taraf chl rha hai

--- backend/controllers/Committee-Controller.js
+++ backend/controllers/Committee-Controller.js
@@ -11,11 +11,7 @@ const addMember = async (req, res) => {
         { members: committee.members }
       );
     } else {
-      const committee = new Committee({ members: [] }).save();
-      await Committee.updateOne(
-        { _id: committee._id },
-        { members: [req.body.id] }
-      );
+      new Committee({ members: [req.body.id] }).save();
     }
     res.status(200).json({
       message: "Successfully added",
@@ -30,7 +26,7 @@ const addMember = async (req, res) => {
 
 const getMembers = async (req, res) => {
   try {
-    const members = await Committee.findOne();
+    const members = await Committee.findOne().populate("members");
     res.status(200).json(members);
   } catch (err) {
     console.log(err);
