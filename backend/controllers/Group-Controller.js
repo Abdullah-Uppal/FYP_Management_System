1626f48a91094edecce27886ea8ec1afe85ccd7f
Abdullah Yaqub <81874639+AbdullahYaqub72@users.noreply.github.com>
2023-01-04 12:19:09 +0500

    [NEW] Group Controllers

--- backend/controllers/Group-Controller.js
+++ backend/controllers/Group-Controller.js
@@ -1,5 +1,5 @@
 const Group = require('../models/Group');
-
+const Person = require('../models/Person');
 
 const create = async (req, res) => {
   try {
@@ -21,6 +21,92 @@ const create = async (req, res) => {
     });
   }
 }
+
+const create_empty_group = async (req, res) => {
+  try {
+    const group = new Group({
+      project: null,
+      students: [],
+      advisors: [],
+    });
+    await group.save();
+    return res.status(200).json({
+      groupid: group._id,
+    });
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({
+      groupid: null,
+    });
+  }
+}
+
+// controller to add students to a group of id
+const add_students = async (req, res) => {
+  try {
+    const { groupid, students } = req.body;
+    const group = await Group.findOne({
+      _id: groupid,
+    });
+    group.students = students;
+    await group.save();
+    return res.status(200).json({
+      message: "Successfully Added Students",
+    });
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Internal Server Error"
+    });
+  }
+}
+
+// controller to add advisors to a group of id
+const add_advisors = async (req, res) => {
+  try {
+    const { groupid, advisors } = req.body;
+    const group
+    = await Group.findOne({
+      _id: groupid,
+    });
+    group.advisors = advisors;
+    await group.save();
+    return res.status(200).json({
+      message: "Successfully Added Advisors",
+    });
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Internal Server Error"
+    });
+  }
+}
+
+// controller to add project to a group of id
+const add_project = async (req, res) => {
+  try {
+    const { groupid, project } = req.body;
+    const group
+    = await Group.findOne({
+      _id: groupid,
+    });
+    group.project = project;
+    await group.save();
+    return res.status(200).json({
+      message: "Successfully Added Project",
+    });
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Internal Server Error"
+    });
+  }
+}
+
 
 const all = async (req, res) => {
   try {
@@ -74,8 +160,37 @@ const update = async (req, res) => {
   }
 }
 
+const getStudentGroup = async (req, res) => {
+  try {
+    const groups = await Group.find();
+    if (groups) {
+      const studentGroups = groups.filter(
+        (group) => {
+          return group.students.includes(req.params.id);
+        }
+      );
+      return res.status(404).json(studentGroups[0]);
+    }
+    else {
+      return res.status(200).json(null);
+    }
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Internal Server Error",
+    })
+  }
+}
+
+
 exports.createGroup = create;
 exports.allGroups = all;
 exports.oneGroup = one;
 exports.deleteGroup = remove;
 exports.updateGroup = update;
+exports.create_empty_group = create_empty_group;
+exports.add_students = add_students;
+exports.add_advisors = add_advisors;
+exports.add_project = add_project;
+exports.getStudentGroup = getStudentGroup;
--- backend/routes/grouproutes.js
+++ backend/routes/grouproutes.js
@@ -7,5 +7,9 @@ router.get('/getGroups', GroupController.allGroups);
 router.get('/getOneGroup/:id', GroupController.oneGroup);
 router.delete('/deleteGroup/:id', GroupController.deleteGroup);
 router.put('/updateGroup/:id', GroupController.updateGroup);
-
+router.get('/createGroup', GroupController.create_empty_group);
+router.post('/addStudentsToGroup/:id', GroupController.add_students);
+router.post('/addAdvisorsToGroup/:id', GroupController.add_advisors);
+router.post('/addProjectToGroup/:id', GroupController.add_project);
+router.get('/getStudentGroup/:id', GroupController.getStudentGroup);
 module.exports = router;
